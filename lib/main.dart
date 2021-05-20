f05e80e516994c3329612443b4701a338eb49855
Lilliyaa <liliia.korotina@nure.ua>
2021-05-20 19:11:20 +0300

    bag fixed

--- lib/main.dart
+++ lib/main.dart
@@ -13,10 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context){
     return MaterialApp(
       debugShowCheckedModeBanner: false, //добавлено. Не знаю, что это значит
-      //theme: myLightTheme,
-      theme: ThemeData(
-          primaryColor: Color.fromRGBO(252, 243, 227, 1),
-          scaffoldBackgroundColor: Color.fromRGBO(252, 243, 227, 1)),
+      theme: myLightTheme,
       title: "Добро пожаловать в \"Твій рух\"",
       //title: "Bottom Nav Bar",
       //home: Nav(),
