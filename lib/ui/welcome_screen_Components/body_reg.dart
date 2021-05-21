c2cb11e017425f621ae79fa2a3d5ad1e8c4878b8
StephniO <stefni.ohu@nure.ua>
2021-05-21 16:33:08 +0300

    Registration don't make crush

--- lib/ui/welcome_screen_Components/body_reg.dart
+++ lib/ui/welcome_screen_Components/body_reg.dart
@@ -3,8 +3,6 @@ import 'package:flutter_apptest/ui/welcome_screen_Components/rounded_input_field
 import 'package:flutter_apptest/ui/welcome_screen_Components/round_btn.dart';
 import 'package:flutter/src/widgets/icon.dart';
 
-import '../Nav.dart';
-
 class Body extends StatelessWidget {
   final Widget child;
 
@@ -15,57 +13,49 @@ class Body extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
+    //Size size = MediaQuery.of(context).size;
     return SafeArea(
       child: Scaffold(
-        drawer: Drawer(),
-        appBar: MyCustomAppBar(
-          height: 100,
-        ),
-        body: Container(
-          //margin: EdgeInsets.symmetric(vertical: 100),
-          height: size.height,
-          width: double.infinity,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              RoundedInputField(
-                hintText: "Прізвище",
-                onChanged: (value){} ,
-              ),
-              RoundedInputField(
-                hintText: "Ім'я",
-                  icon: Icons.accessibility,
-                onChanged: (value){} ,
-              ), RoundedInputField(
-                hintText: "Електронна адреса",
-                icon: Icons.alternate_email_rounded,
-                onChanged: (value){} ,
-              ),  RoundedInputField(
-                hintText: "Телефон",
-                icon: Icons.add_ic_call,
-                onChanged: (value){} ,
-              ),
-              RoundPassword(onChange: (value){},),
-              SizedBox(height: size.height*0.1),
-              RoundedButton(
-                text: "Зареєструватися",
-                press: (){
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context){
-                            return Nav();
-                          }
-                      )
-                  );
-                },
-              ),
-              SizedBox(height: size.height*0.01),
-              OrDivider(),
-            ],
+          drawer: Drawer(),
+          appBar: MyCustomAppBar(
+            height: 100,
           ),
-        )
+          body: Container(
+            //margin: EdgeInsets.symmetric(vertical: 100),
+            //height: size.height,
+            width: double.infinity,
+            child: ListView (
+              //mainAxisAlignment: MainAxisAlignment.center,
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              children: <Widget>[
+                RoundedInputField(
+                  hintText: "Прізвище",
+                  onChanged: (value){} ,
+                ),
+                RoundedInputField(
+                  hintText: "Ім'я",
+                  icon: Icons.accessibility,
+                  onChanged: (value){} ,
+                ), RoundedInputField(
+                  hintText: "Електронна адреса",
+                  icon: Icons.alternate_email_rounded,
+                  onChanged: (value){} ,
+                ),  RoundedInputField(
+                  hintText: "Телефон",
+                  icon: Icons.add_ic_call,
+                  onChanged: (value){} ,
+                ),
+                RoundPassword(onChange: (value){},),
+                // SizedBox(height: size.height*0.1),
+                RoundedButton(
+                  text: "Зареєструватися",
+                  press: (){},
+                ),
+                // SizedBox(height: size.height*0.01),
+                OrDivider(),
+              ],
+            ),
+          )
       ),
     );
   }
@@ -76,26 +66,26 @@ class OrDivider extends StatelessWidget{
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Container(
-      width: size.width*0.8,
-      child: Row(
-        children: <Widget>[
-          Expanded(child:
-          Divider(color: Color.fromRGBO(139, 103, 46, 1),
-                  height: 1.5,)
-          ),
-          Text("АБО",
+        width: size.width*0.8,
+        child: Row(
+          children: <Widget>[
+            Expanded(child:
+            Divider(color: Color.fromRGBO(139, 103, 46, 1),
+              height: 1.5,)
+            ),
+            Text("АБО",
 
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              color: Color.fromRGBO(139, 103, 46, 1),
-            fontWeight: FontWeight.w600,
-          ),),
-          Expanded(child:
-          Divider(color: Color.fromRGBO(139, 103, 46, 1),
-            height: 1.5,)
-          ),
-        ],
-      )
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Color.fromRGBO(139, 103, 46, 1),
+                fontWeight: FontWeight.w600,
+              ),),
+            Expanded(child:
+            Divider(color: Color.fromRGBO(139, 103, 46, 1),
+              height: 1.5,)
+            ),
+          ],
+        )
     );
   }
 }
@@ -119,8 +109,8 @@ class MyCustomAppBar extends StatelessWidget implements PreferredSizeWidget {
           child: Padding(
             padding: EdgeInsets.all(5),
             child: Text("Швидка реєстрація",
-                textAlign: TextAlign.center,
-                style: TextStyle(fontWeight: FontWeight.bold, fontFamily: 'Roboto', fontSize: 20 ),
+              textAlign: TextAlign.center,
+              style: TextStyle(fontWeight: FontWeight.bold, fontFamily: 'Roboto', fontSize: 20 ),
             ),
           ),
         ),
@@ -131,24 +121,3 @@ class MyCustomAppBar extends StatelessWidget implements PreferredSizeWidget {
   @override
   Size get preferredSize => Size.fromHeight(height);
 }
-/*
- @override
-  Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
-    return Container(
-      margin: EdgeInsets.symmetric(vertical: 100),
-      height: size.height,
-      width: double.infinity,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Text("Швидка реєстрація",
-            style: TextStyle(fontWeight: FontWeight.bold, fontFamily: 'Roboto', fontSize: 20 ),
-          ),
-                  ],
-      ),
-    );
-  }
-*
-*
-* */
