0cec2d9ef2b438273d2be175e2ac53f073d4f134
Lilliyaa <80325779+Lilliyaa@users.noreply.github.com>
2021-05-22 22:20:46 +0300

    Update theme.dart

--- lib/theme.dart
+++ lib/theme.dart
@@ -3,39 +3,30 @@ import 'package:flutter/material.dart';
 ThemeData myLightTheme = ThemeData(
     primaryColor: Color.fromRGBO(252, 243, 227, 1),
     bottomAppBarColor: Color(0xff4b4a4a),
-    textTheme: TextTheme(
-      headline1: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
-      headline2: TextStyle(
-        //shadows: ShadowElement(0),
-        color: Color.fromRGBO(143, 141, 141, 1),
-        fontSize: 20,
-        fontWeight: FontWeight.w900,
-      ),
-      headline3: TextStyle(
-        //shadows: ShadowElement(0),
-        color: Color.fromRGBO(143, 141, 141, 1),
-        fontSize: 14,
-        fontWeight: FontWeight.w900,
-      ),
-      headline6: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
-      bodyText1: TextStyle(
-          fontSize: 16.0, fontFamily: 'Roboto', color: Color(0xff4b4a4a)),
 
+    textTheme: TextTheme(
+
+      headline1: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
+
+      //for welcomeScreen
+      headline4: TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold),
+
+      headline6: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
+
+      bodyText1: TextStyle(fontSize: 16.0, fontFamily: 'Roboto', color: Color(0xff4b4a4a)
+      ),
 
       //for question`s text
-      bodyText2: TextStyle(
-        fontSize: 18.0,
-        fontFamily: 'Roboto',
-        color: Colors.black,
-        shadows: <Shadow>[
-          Shadow(
-            offset: Offset(2.0, 2.0),
-            blurRadius: 3.0,
-            color: Color.fromRGBO(140, 140, 140, 0.5),
-          ),
-        ],
+      bodyText2: TextStyle(fontSize: 18.0, fontFamily: 'Roboto', color: Colors.black, shadows: <Shadow>[
+        Shadow(
+          offset: Offset(2.0, 2.0),
+          blurRadius: 3.0,
+          color:Color.fromRGBO(140, 140, 140, 0.5),
+        ),
+      ],
       ),
     ),
+
     appBarTheme: AppBarTheme(
       backgroundColor: Color.fromRGBO(254, 187, 87, 1),
       titleTextStyle: TextStyle(
@@ -46,11 +37,12 @@ ThemeData myLightTheme = ThemeData(
       ),
       //color: Color.fromRGBO(254, 187, 87, 0)
     ),
+
     bottomNavigationBarTheme: BottomNavigationBarThemeData(
       unselectedItemColor: Color.fromRGBO(140, 140, 140, 1),
       selectedItemColor: Colors.white,
       backgroundColor: Color(0xff4b4a4a),
       type: BottomNavigationBarType.fixed,
     )
-);
 
+);
